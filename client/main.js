3151216a5a909c1f70316e49d331a5a73654d5b9
Carey Casile <carey.casile@gmail.com>
2017-11-18 14:02:07 -0500

    connected server/clientside

--- client/main.js
+++ client/main.js
@@ -1,1 +1,16 @@
-console.log('log from main.js/client');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+
+Meteor.startup(function () {
+	let name = 'Chip';
+	let title = 'This is a title';
+	let jsx = (
+		<div>
+			<h1>{title}</h1>
+			<p>Hello {name}</p>
+			<p>This is the second paragraph</p>
+		</div>
+	);
+	ReactDOM.render(jsx, document.getElementById('app'));
+});
