fb1080938b405b330dab1d0ff69e6985856be977
Carey Casile <carey.casile@gmail.com>
2017-11-20 14:57:14 -0500

    added mongo server

--- client/main.js
+++ client/main.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
-
-const players = [{
-	_id: 1,
-	name: 'Carey',
-	score: 0
-},{
-	_id: 2,
-	name: 'Sarah',
-	score: 0
-},{
-	_id: 3,
-	name: 'Chris',
-	score: 0
-}];
+import {Tracker} from 'meteor/tracker';
+import {Players} from './../imports/api/players';
 
 const renderPlayers = function (players) {
 
 		return players.map(function(player){
 			return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
 		});
+
 }
 
 Meteor.startup(function () {
-	let name = 'Chip';
-	let title = 'This is a title';
-	let jsx = (
-		<div>
-			<h1>{title}</h1>
-			<p>Hello {name}</p>
-			<p>This is the second paragraph</p>
-			{renderPlayers(players)}
-		</div>
-	);
-	ReactDOM.render(jsx, document.getElementById('app'));
+
+	Tracker.autorun(function () {
+		let players = Players.find().fetch();
+		console.log(players);
+
+		let title = 'Score Keep';
+		let jsx = (
+			<div>
+				<h1>{title}</h1>
+				{renderPlayers(players)}
+			</div>
+		);
+		ReactDOM.render(jsx, document.getElementById('app'));
+	});
+
+	let userInput = prompt('input your name:');
+	Players.insert({
+		name: userInput,
+		score: 0
+	});
 });
